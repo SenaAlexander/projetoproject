903d68bce84335da660d55c5e2cedcdf9ae1a2cb
Jab1roca <gabriel.silva2499@etec.sp.gov.br>
2023-03-15 22:15:27 -0300

    firts commit

---
+++ app/src/main/java/com/example/calculadora/Calculadora.kt
@@ -0,0 +1,15 @@
+package com.example.calculadora
+
+class Calculadora {
+    fun sum(a: Double, b: Double) : Double = a + b
+
+    fun subtract(a: Double, b: Double) : Double = a - b
+
+    fun multiply(a: Double, b: Double) : Double = a * b
+
+    fun divide(a: Double, b: Double) : Double {
+        if (b == 0.0) throw IllegalArgumentException("Divisao por zero nao pode!!")
+        return a / b
+    }
+
+}
